7f27c8182e07bc85f0b8b2bf0fbc3f4dbc973097
Julio Cesar Coutinho <98652054+juliocesarcoutinho@users.noreply.github.com>
2022-05-13 12:42:00 -0300

    Projeto

---
+++ Drogaria/src/main/java/br/com/julio/drogaria/util/HibernateUtil.java
@@ -0,0 +1,57 @@
+package br.com.julio.drogaria.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.jdbc.ReturningWork;
+
+public class HibernateUtil {
+	private static SessionFactory fabricaDeSessoes = criarFabricaDeSessoes();
+
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getFabricaDeSessoes() {
+		return fabricaDeSessoes;
+	}
+
+	public static Connection getConexao() {
+		Session sessao = fabricaDeSessoes.openSession();
+
+		Connection conexao = sessao.doReturningWork(new ReturningWork<Connection>() {
+			@Override
+			public Connection execute(Connection conn) throws SQLException {
+				return conn;
+			}
+		});
+
+		return conexao;
+	}
+
+	static SessionFactory criarFabricaDeSessoes() {
+		try {
+			// Create registry
+			registry = new StandardServiceRegistryBuilder().configure().build();
+
+			// Create MetadataSources
+			MetadataSources sources = new MetadataSources(registry);
+
+			// Create Metadata
+			Metadata metadata = sources.getMetadataBuilder().build();
+
+			// Create SessionFactory
+			sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+			return sessionFactory;
+		} catch (Throwable ex) {
+			System.err.println("A fábrica de sessões não pode ser criada." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+}
