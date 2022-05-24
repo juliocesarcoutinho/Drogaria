4fe5ce7bf4a04e0f56b1913b5552f599f3a91dda
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-05-24 11:35:26 -0300

    Atualização Cidade

--- src/main/java/br/com/julio/drogaria/bean/CidadeBean.java
+++ src/main/java/br/com/julio/drogaria/bean/CidadeBean.java
@@ -9,7 +9,9 @@ import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 import br.com.julio.drogaria.dao.CidadeDAO;
+import br.com.julio.drogaria.dao.EstadoDAO;
 import br.com.julio.drogaria.domain.Cidade;
+import br.com.julio.drogaria.domain.Estado;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -17,37 +19,77 @@ import br.com.julio.drogaria.domain.Cidade;
 public class CidadeBean implements Serializable {
 	private Cidade cidade;
 	private List<Cidade> cidades;
-	
+	private List<Estado> estados;
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
 	public Cidade getCidade() {
 		return cidade;
 	}
-	
+
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
-	
+
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
-	
+
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
-	
-	@PostConstruct ()
+
+	@PostConstruct()
 	public void listar() {
 		try {
-			CidadeDAO cidadeDAO= new CidadeDAO();
+			CidadeDAO cidadeDAO = new CidadeDAO();
 			cidades = cidadeDAO.listar();
-			
+
 		} catch (RuntimeException erro) {
 			Messages.addFlashGlobalError("Erro Ao Listar as Cidades");
 			erro.printStackTrace();
 		}
-		
+
+	}
+
+	public void novo() {
+
+		try {
+			cidade = new Cidade();
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Erro ao Gerar uma nova Cidade");
+			erro.printStackTrace();
+		}
+
 	}
 	
-	public void novo() {
-		cidade = new Cidade();
+	public void salvar() {
+		try {
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidadeDAO.merge(cidade);
+			
+			cidade = new Cidade();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+			
+			cidades =  cidadeDAO.listar();
+			
+			Messages.addGlobalInfo("Cidade Salva com Sucesso");
+			
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Erro ao Salvar a Cidade");
+			erro.printStackTrace();
+		}
 	}
 }
