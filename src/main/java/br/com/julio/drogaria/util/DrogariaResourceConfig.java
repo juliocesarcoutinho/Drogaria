1d8363ae3c5b40d52af0baa09b86d3d7494554e5
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-05-31 16:44:41 -0300

    Criação do Resource Config ResTfull
    Criação do Configuração do Restiful

---
+++ src/main/java/br/com/julio/drogaria/util/DrogariaResourceConfig.java
@@ -0,0 +1,12 @@
+package br.com.julio.drogaria.util;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class DrogariaResourceConfig extends ResourceConfig {
+	public DrogariaResourceConfig() {
+		packages("br.com.julio.drogaria.service");
+	}
+}
