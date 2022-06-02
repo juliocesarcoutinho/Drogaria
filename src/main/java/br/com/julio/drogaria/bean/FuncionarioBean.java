89afe24fa81cc0c45fa547898f8aa77627679912
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-06-02 17:30:20 -0300

    Create FuncionarioBean.java
    Criação Funcionario Bean

---
+++ src/main/java/br/com/julio/drogaria/bean/FuncionarioBean.java
@@ -0,0 +1,43 @@
+package br.com.julio.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.julio.drogaria.domain.Funcionario;
+import br.com.julio.drogaria.domain.Pessoa;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class FuncionarioBean implements Serializable {
+	private Funcionario funcionario;
+	private List<Funcionario> funcionarios;
+	private List<Pessoa> pessoas;
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+}
