174f055aa0bc9475f784c2be6d0c36ae76320b27
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-06-03 17:17:32 -0300

    Update ClienteDAO.java
    Criação da listagem de forma que seja ordenado

--- src/main/java/br/com/julio/drogaria/dao/ClienteDAO.java
+++ src/main/java/br/com/julio/drogaria/dao/ClienteDAO.java
@@ -1,7 +1,29 @@
 package br.com.julio.drogaria.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
 import br.com.julio.drogaria.domain.Cliente;
+import br.com.julio.drogaria.util.HibernateUtil;
 
-public class ClienteDAO extends GenericDAO<Cliente>{
+public class ClienteDAO extends GenericDAO<Cliente> {
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public List<Cliente> listarOrdenado() {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try {
+			Criteria consulta = sessao.createCriteria(Cliente.class);
+			consulta.createAlias("pessoa", "p");
+			consulta.addOrder(Order.asc("p.nome"));
+			List<Cliente> resultado = consulta.list();
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		} finally {
+			sessao.close();
+		}
 
+	}
 }
