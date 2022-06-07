cccfc00b1923564ddb213340f7e462d17f04f9a1
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-06-07 15:18:04 -0300

    Update ProdutoBean.java
    Inclusão do metodo pra imprimir relatório

--- src/main/java/br/com/julio/drogaria/bean/ProdutoBean.java
+++ src/main/java/br/com/julio/drogaria/bean/ProdutoBean.java
@@ -6,13 +6,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
@@ -21,6 +25,11 @@ import br.com.julio.drogaria.dao.FabricanteDAO;
 import br.com.julio.drogaria.dao.ProdutoDAO;
 import br.com.julio.drogaria.domain.Fabricante;
 import br.com.julio.drogaria.domain.Produto;
+import br.com.julio.drogaria.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -155,6 +164,23 @@ public class ProdutoBean implements Serializable {
 
 		} catch (IOException erro) {
 			Messages.addGlobalError("Erro ao fazer upload da imagem");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void imprimir() {
+		try {
+			String caminho = Faces.getRealPath("/reports/produto.jasper");
+
+			Map<String, Object> parametros = new HashMap<>();
+
+			Connection conexao = HibernateUtil.novaConexao();
+
+			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+			
+			JasperPrintManager.printReport(relatorio, true);
+		} catch (JRException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
 			erro.printStackTrace();
 		}
 	}
