60c74db442a18eef60b7619453412dc8bd38abab
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-06-09 11:23:27 -0300

    Update UsuarioDAO.java

--- src/main/java/br/com/julio/drogaria/dao/UsuarioDAO.java
+++ src/main/java/br/com/julio/drogaria/dao/UsuarioDAO.java
@@ -1,7 +1,36 @@
 package br.com.julio.drogaria.dao;
 
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
 import br.com.julio.drogaria.domain.Usuario;
+import br.com.julio.drogaria.util.HibernateUtil;
 
 public class UsuarioDAO extends GenericDAO<Usuario> {
-
+	@SuppressWarnings("deprecation")
+	public Usuario autenticar(String cpf, String senha) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		
+		try {
+			Criteria consulta = sessao.createCriteria(Usuario.class);
+			consulta.createAlias("pessoa", "p");
+					
+			consulta.add(Restrictions.eq("p.cpf", cpf));
+			
+			SimpleHash hash = new SimpleHash("md5", senha);
+			consulta.add(Restrictions.eq("senha", hash.toHex()));
+			
+			Usuario resultado = (Usuario) consulta.uniqueResult();
+			return resultado;
+					
+			
+		} catch (RuntimeException erro) {
+			throw erro;
+		}finally {
+			sessao.close();
+		}
+		
+	}
 }
